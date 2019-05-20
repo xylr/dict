c149c21e201eb5e32500194cd0b75b26abeda7a7
Tedu <2389196764@qq.com>
2019-05-20 18:22:10 +0800

    login and enter is done

--- dict_client.py
+++ dict_client.py
@@ -1,5 +1,7 @@
 from socket import *
 from sys import *
+from time import *
+
 
 def login(s):
     s.send("注册".encode())
@@ -29,18 +31,66 @@ def login(s):
                     except Exception as e:
                         print(e)
 def enter(s):
-    s.send("登录".decode())
+    s.send("登录".encode())
     data = s.recv(1024).decode()
-    if data = "OK":
+    if data == "OK":
         while True:
-            name = str(input("请输入您的用户名"))
-            
+            name = str(input("请输入您的用户名:"))
+            s.send(name.encode())
+            data = s.recv(1024).decode()
+            if data == 'Please login first':
+                print("请先注册")
+                return 'false'
+            elif data == "name is not exist,please input again":
+                print("用户名错误,请重新输入")
+                continue
+            else:
+                while True:
+                    password = input("请输入密码:")
+                    s.send(password.encode())
+                    data = s.recv(1024).decode()
+                    if data == "Successfully enter":
+                        print("登陆成功")
+                        return "OK"
+                    elif data == "password is wrong please input again":
+                        print("密码错误,请重新输入")
+
+def check_word(s):
+    while True:
+        word_name = str(input("请输入所要查询的单词"))
+        s.send(word_name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def main():
     str1 = '''            1.登录
             2.注册
             3.退出
             请输入对应序号执行相应功能
     '''
+    str2 = '''            1.查单词
+            2.历史记录
+            3.退出到一级界面
+            请输入对应序号执行相应功能
+    '''
+
     #!/usr/bin/env python3
     ip = argv[1]
     port = int(argv[2])
@@ -53,13 +103,25 @@ def main():
         print(str1)
         a = int(input('请选择:'))
         if a == 1:
-            enter(s)
+            message = enter(s)
+            if message == "false":
+                print(str1)
+            elif message == "OK":
+                break
         elif a == 2:
             login(s)
             print(str1)
         elif a == 3:
             s.send(b'quit')
             exit("程序退出")
+    while True:
+        print(str2)
+        a = int(input("请选择:"))
+        if a == 1:
+            check_word(s)
+        if a == 2:
+            check_history(s)
+
 
 if __name__ == "__main__":
     main()
--- dict_server.py
+++ dict_server.py
@@ -6,18 +6,18 @@ import signal
 
 def login(c,db,cur):
     c.send(b'OK')
-    try:
-        # db = connect( host="localhost",user="root",
-        # password="123456",database="login",
-        # charset="utf8")
-        # cur = db.cursor()
+    try:#查询login1表中的用户注册信息
         cur.execute('select name from login1')
+        #拿到所有的用户姓名
         t = cur.fetchall()
+        #将拿到的元组转换为列表方便进行增删改查
         l1 = list(t)
         l2 = []
+        #将所有的姓名加入到新列表方便进行姓名的是否重复查询
         for i in l1:
             l2.append(i[0])
         while True:
+            #循环接收用户名,并判断是否重复
             name = c.recv(1024).decode()
             if not name:
                 sys.exit("注册失败")
@@ -25,19 +25,22 @@ def login(c,db,cur):
                 c.send(b'name is available,Please enter your password')
             else:
                 for i in l2:
+                    #若用户名存在则重新进行用户名注册
                     if i == name:
                         c.send(b'name is already exist')
                         continue
             c.send(b'name is available,Please enter your password')
             while True:
+                #用l4列表来判断两次接收到的密码是否一致
                 l4 = []
                 for i in range(2):
                     password = c.recv(1024).decode()
                     l4.append(password)
                 if l4[0] != l4[1]:
+                    #密码不相同则重新输入
                     c.send(b"password is not same Please enter twice")
                     continue
-                else:
+                else:#密码相同将用户信息插入到用户信息表
                     c.send(b'successfully login')
                     cur.execute('insert into login1(name,password) \
                                 values(%s,%s);',[name,l4[0]])
@@ -48,31 +51,60 @@ def login(c,db,cur):
         print(e)
 def enter(c,cur):
     c.send(b"OK")
-    cur.execute('select name from login1')
+    #将姓名和密码一起查出来
+    cur.execute('select name,password from login1')
+    #存入元组并强转为列表方便增删改查
     t = cur.fetchall()
     l1 = list(t)
     l2 = []
     for i in l1:
         l2.append(i[0])
     while True:
+        #接收用户名
         name = c.recv(1024).decode()
-            if not name:
-                sys.exit("注册失败")
-            if l2 = []:
-                c.send(b'Please login first')     
-def handle(c,db1,cur1):
-    try:
+        if not name:
+            sys.exit("登录失败")
+        if l2 == []:
+            c.send(b'Please login first')
+            return
+        else:
+            if name in l2:
+                c.send(b"name is right")
+                break
+            else:
+                c.send(b'name is not exist,please input again')
+    while True:
+        #根据用户名匹配相对应的密码进行和客户端的密码校对
+        cur.execute('select password from login1 where name = %s',[name])
+        t = cur.fetchone()
+        password = c.recv(1024).decode()
+        if not password:
+            sys.exit("登录失败")
+        if t[0] == password:
+            c.send(b"Successfully enter")
+            return name
+        if t[0] != password:
+            c.send(b'password is wrong please input again')
+def handle(c):
+    try:#连接用户信息数据库,进行相应的操作
         db1 = connect( host="localhost",user="root",
                     password="123456",database="login",
                     charset="utf8")
+        #创建cur1对象来操作数据库
         cur1 = db1.cursor()
+        #连接历史记录表,进行历史记录的读取
+        db2 = connect( host="localhost",user="root",
+                    password="123456",database="login",
+                    charset="utf8")
+        cur2 = db2.cursor()
+        #接收用户发送的请求
         data = c.recv(1024).decode()
         if data == "注册":
             login(c,db1,cur1)
         if data == "quit":
             print("用户退出")
-        if data = "登录":
-            enter(c)
+        if data == "登录":
+            name = enter(c,cur1)
     finally:
         cur1.close()
         db1.close()
@@ -91,6 +123,7 @@ def main():
         except:
             print("请重新连接")
             continue
+        #创建子进程接收客户端请求,主进程接收客户端连接
         p = Process(target = handle,args = (c,))
         p.daemon = True
         p.start()
