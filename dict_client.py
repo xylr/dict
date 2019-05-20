da3df81e93bb22ab1a91f3a9d52304698d5c2837
Tedu <2389196764@qq.com>
2019-05-20 16:48:17 +0800

    score1

--- dict_client.py
+++ dict_client.py
@@ -1,26 +1,26 @@
 from socket import *
+from sys import *
 
-
-def login2():
+def login(s):
     s.send("注册".encode())
     data = s.recv(1024).decode()
-    if data = 'OK':
+    if data == 'OK':
         while True:
-            name = str(input("请输入用户名"))
+            name = str(input("请输入用户名:"))
             s.send(name.encode())
             data = s.recv(1024).decode()
-            if data = 'name is already exist':
+            if data == 'name is already exist':
                 print("用户名重复,请重新输入")
                 continue
             else:
                 while True:
                     try:
-                        password = str(input("请输入密码"))
+                        password = str(input("请输入密码:"))
                         s.send(bytes(password,'utf-8'))
-                        password = str(input("请再次输入密码"))
+                        password = str(input("请再次输入密码:"))
                         s.send(bytes(password,'utf-8'))
                         data = s.recv(1024).decode()
-                        if data = 'password is not same Please enter twice':
+                        if data == 'password is not same Please enter twice':
                             print("两次密码不一致,请重新输入")
                             continue
                         else:
@@ -28,25 +28,38 @@ def login2():
                             return
                     except Exception as e:
                         print(e)
+def enter(s):
+    s.send("登录".decode())
+    data = s.recv(1024).decode()
+    if data = "OK":
+        while True:
+            name = str(input("请输入您的用户名"))
+            
 def main():
-    str1 = '''1.登录
-2.注册
-3.退出
+    str1 = '''            1.登录
+            2.注册
+            3.退出
+            请输入对应序号执行相应功能
     '''
     #!/usr/bin/env python3
     ip = argv[1]
-    port = argv[2]
+    port = int(argv[2])
     addr = (ip,port)
+    print(addr)
     s = socket()
     s.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
     s.connect(addr)
-    print(str1)
-    a = int(input('请选择'))
-    if a == 1:
-        s.send("登陆".encode())
-    elif a == 2:
-        login()
+    while True:
         print(str1)
-    
+        a = int(input('请选择:'))
+        if a == 1:
+            enter(s)
+        elif a == 2:
+            login(s)
+            print(str1)
+        elif a == 3:
+            s.send(b'quit')
+            exit("程序退出")
+
 if __name__ == "__main__":
     main()
--- dict_server.py
+++ dict_server.py
@@ -2,29 +2,32 @@ from socket import *
 from multiprocessing import Process
 from pymysql import *
 import sys
+import signal
 
-def login():
+def login(c,db,cur):
     c.send(b'OK')
     try:
-        db = connect( host="localhost",user="root",
-        password="123456",database="login",
-        charset="utf8")
-        cur = db.cursor()
-        cur.excute('select name from login1')
+        # db = connect( host="localhost",user="root",
+        # password="123456",database="login",
+        # charset="utf8")
+        # cur = db.cursor()
+        cur.execute('select name from login1')
         t = cur.fetchall()
         l1 = list(t)
         l2 = []
         for i in l1:
             l2.append(i[0])
-        
         while True:
             name = c.recv(1024).decode()
             if not name:
                 sys.exit("注册失败")
-            for i in l2:
-                if i == name:
-                    c.send(b'name is already exist')
-                    continue
+            if l2 == []:
+                c.send(b'name is available,Please enter your password')
+            else:
+                for i in l2:
+                    if i == name:
+                        c.send(b'name is already exist')
+                        continue
             c.send(b'name is available,Please enter your password')
             while True:
                 l4 = []
@@ -32,34 +35,55 @@ def login():
                     password = c.recv(1024).decode()
                     l4.append(password)
                 if l4[0] != l4[1]:
-                    c.send("password is not same Please enter twice")
+                    c.send(b"password is not same Please enter twice")
                     continue
                 else:
                     c.send(b'successfully login')
-                    cur.excute('insert into login1("name","password" \
-                        values(%s%s);',[name,l4[0]])
+                    cur.execute('insert into login1(name,password) \
+                                values(%s,%s);',[name,l4[0]])
                     db.commit()
+                    print(name,"用户注册成功")
                     return
     except Exception as e:
         print(e)
+def enter(c,cur):
+    c.send(b"OK")
+    cur.execute('select name from login1')
+    t = cur.fetchall()
+    l1 = list(t)
+    l2 = []
+    for i in l1:
+        l2.append(i[0])
+    while True:
+        name = c.recv(1024).decode()
+            if not name:
+                sys.exit("注册失败")
+            if l2 = []:
+                c.send(b'Please login first')     
+def handle(c,db1,cur1):
+    try:
+        db1 = connect( host="localhost",user="root",
+                    password="123456",database="login",
+                    charset="utf8")
+        cur1 = db1.cursor()
+        data = c.recv(1024).decode()
+        if data == "注册":
+            login(c,db1,cur1)
+        if data == "quit":
+            print("用户退出")
+        if data = "登录":
+            enter(c)
     finally:
-        cur.close()
-        db.close()
-def handle():
-    data = c.recv(1024).decode()
-    if data == "注册":
-        login()
-
-
-
-
+        cur1.close()
+        db1.close()
 
 
 def main():
+    signal.signal(signal.SIGCHLD,signal.SIG_IGN)
     addr = ('0.0.0.0',8888)
     s = socket()
     s.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
-    s.bind((addr))
+    s.bind(addr)
     s.listen(5)
     while True:
         try:
@@ -67,9 +91,9 @@ def main():
         except:
             print("请重新连接")
             continue
-        p = Process(target = handle)
+        p = Process(target = handle,args = (c,))
+        p.daemon = True
         p.start()
-        p.Daemon = True
 
 if __name__ == "__main__":
     main()
